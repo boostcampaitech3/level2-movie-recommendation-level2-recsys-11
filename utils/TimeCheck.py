b65740db279d39fbc25851c39ea0842d4ba1fd27
iksadNorth <iksadnorth@gmail.com>
2022-03-21 05:47:16 +0000

    feat: > TimeCheck.py 파일 추가
    bulid:
    > utils 폴더 생성
    위 폴더는 기존 코드를 훼손시키지 않기 위해 생성.

---
+++ utils/TimeCheck.py
@@ -0,0 +1,74 @@
+# %%
+from time import time
+from time import sleep
+
+# %%
+class TimeCheck():
+    FN_PRINT = print
+    def __init__(self, flag=True) -> None:
+        self.queue = []
+        self.flag = flag
+    
+    def __getattribute__(self, __name: str):
+        if object.__getattribute__(self, 'flag'):
+            return object.__getattribute__(self, __name)
+        else:
+            return lambda *args, **kwargs: None
+    
+    def __call__(self, name_flag:str):
+        self.mark_(name_flag)    
+    
+    def mark(self, name_flag:str):
+        entity = name_flag, time()
+        self.queue.append(entity)
+        
+    def mark_(self, name_flag:str):
+        self.mark(name_flag)
+        self.FN_PRINT(name_flag)
+    
+    def cal(self):
+        result = {}
+        for i, entity in enumerate(self.queue):
+            if i == 0:
+                previous_entity = entity
+                continue
+            else:
+                name = f"[{previous_entity[0]}] --> [{entity[0]}]"
+                diff = entity[1] - previous_entity[1]
+                result[name] = diff
+                previous_entity = entity
+        
+        return result
+    
+    def print(self, name_flag:str, fn_print=None):
+        self.mark(name_flag)
+        if not fn_print:
+            fn_print = lambda k, v: self.FN_PRINT(f'{k:<50} \t{v:4.2f}초')
+        
+        self.FN_PRINT('=' * 50)
+        for k, v in self.cal().items():
+            fn_print(k, v)
+        self.FN_PRINT('=' * 50)
+
+        self.queue = []
+
+# %%
+if __name__ == '__main__':
+    check = TimeCheck()
+    
+    check.mark('first')
+    sleep(0.5)
+    check.mark('second')
+    sleep(0.5)
+    check.mark_('third')
+    sleep(0.5)
+    check.mark_('fourth')
+    sleep(0.5)
+    check('fifth')
+    sleep(0.5)
+    check('sixth')
+    sleep(0.5)
+    
+    check.print("terminate")
+    
+# %%
